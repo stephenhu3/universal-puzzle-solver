302b0a734c514829943b6a033a76b10db324a23d
stephen group 20 <stephenhu3@gmail.com>
2015-04-07 18:11:05 -0700

    double hash map

--- DoubleHashDict.cpp
+++ DoubleHashDict.cpp
@@ -18,6 +18,7 @@ const int DoubleHashDict::primes[] = {53, 97, 193, 389, 769, 1543, 3079,
 // http://planetmath.org/goodhashtableprimes
 // The -1 at the end is to guarantee an immediate crash if we run off
 // the end of the array.
+#define LARGEST_SIZE_INDEX 26
 
 DoubleHashDict::DoubleHashDict() {
   size_index = 0;
@@ -92,7 +93,57 @@ std::cout << "*** REHASHING " << size;
 // End of "DO NOT CHANGE" Block
 
 
-  // TODO:  Your code goes here...
+  // copy everything from previous table into new table of next prime size
+  // increment size_index, if size_index reached max size, don't resize anymore
+  int oldsize = size;
+  // printf("size index %d\n", size_index);
+
+  if (++size_index == LARGEST_SIZE_INDEX)
+    return;
+  
+  size = primes[size_index];
+  
+  // point to the old table, need to be a new copy
+  bucket* oldtable = new bucket[oldsize]();
+
+  int i;
+  // are these actually new values, or pointer to the old table?
+  for (i = 0; i < oldsize; i++) {
+    oldtable[i].key = table[i].key;
+    oldtable[i].data = table[i].data;
+    oldtable[i].keyID = table[i].keyID;
+  }
+
+  // table points to entirely new array of buckets new size
+  // here we just zeroed the old array!, MISTAKE!!
+  table = new bucket[size]();
+
+  // add back the old values to the new table
+  for (i = 0; i < oldsize; i++) {
+    if (oldtable[i].key != NULL)
+      add(oldtable[i].key, oldtable[i].data);
+    else
+      table[i].key = NULL;
+  }
+
+  /*
+   //For debugging purposes
+  int j;
+  for (j = 0; j < oldsize; j++) {
+    std::cout << "j: " << j << "keyID: " << oldtable[j].keyID << std::endl;
+  }
+
+
+   //For debugging purposes
+  // int j;
+  for (j = 0; j < size; j++) {
+    std::cout << "k: " << j << "keyID: " << table[j].keyID << std::endl;
+  }
+  */
+
+  // delete the old table
+  delete [] oldtable;
+
 
 
 // 221 Students:  DO NOT CHANGE OR DELETE THE NEXT FEW LINES!!!
@@ -112,13 +163,67 @@ bool DoubleHashDict::find(PuzzleState *key, PuzzleState *&pred) {
   // Be sure not to keep calling getUniqId() over and over again!
 
   // TODO:  Your code goes here...
-  return true; // Stub:  Delete this line when you've implemented the function
+  // use key to find slot, then compare the pred
+  string findID = key->getUniqId();
+  int i = 1;
+  int hash_fn1 = hash1(findID);
+  int hash_fn2 = hash2(findID);
+  int doubleHash = i * hash_fn2;
+  int searchIndex = hash_fn1 % size;
+
+  // while not found, keep searching
+  while (table[searchIndex].keyID.compare(findID) != 0) {
+    probes_stats[i]++;
+    doubleHash = (++i) * hash_fn2;
+    searchIndex = (hash_fn1 + doubleHash) % size;
+    // problem is rehash isn't actually changing size
+    // printf("size: %d\n", size);
+    // printf("i: %d\n", i);
+    // printf("number: %d\n", number);
+    if (i >= MAX_STATS) {
+      // WE MUST SET PRED TO NULL SINCE WE DIDNT FIND IT
+      pred = NULL;
+      return false; // could not find it
+    }
+  }
+
+  // found it
+  probes_stats[i]++;
+  pred = table[searchIndex].data; // Got it!  Get the result.
+  return true; 
 }
 
 // You may assume that no duplicate PuzzleState is ever added.
 void DoubleHashDict::add(PuzzleState *key, PuzzleState *pred) {
 
-  // TODO:  Your code goes here...
+  string uniqId = key->getUniqId();
+
+  double loadfactor = (double)number/(double)size;
+  // printf("load factor: %f\n", loadfactor);
+
+  if (loadfactor > 0.5) {
+    rehash();
+  }
+
+  int i = 1;
+  int hash_fn1 = hash1(uniqId);
+  int hash_fn2 = hash2(uniqId);
+  int entry = hash_fn1 % size;
+  int doubleHash = i * hash_fn2;
+
+  while (table[entry].key != NULL) {
+      doubleHash = (++i) * hash_fn2;
+      entry = (hash_fn1 + doubleHash) % size;
+      
+      // don't add if exceeds or equal max probes
+      if (i >= MAX_STATS)
+        return;
+  }
+
+  table[entry].key = key;
+  table[entry].data = pred;
+  table[entry].keyID = uniqId;
+  number++;
 }
 
 #endif 
